0792cc263f52916a9cef77f8376d0c2abbbb7d19
PirngruberChris <christianpirngruber3@gmail.com>
2021-01-16 14:07:37 +0100

    Added a CheckBox to the ListView

--- code/Reciply/Reciply.Android/obj/Debug/90/android/environment.x86_64.s
+++ code/Reciply/Reciply.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"f59996ef-43a1-4e9e-9896-3b514cfcd107"
+	.asciz	"88ab4646-5010-4f92-aef4-21b0294bb3e9"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- code/Reciply/Reciply/MainPage.xaml.cs
+++ code/Reciply/Reciply/MainPage.xaml.cs
@@ -16,6 +16,7 @@ namespace Reciply
             InitializeComponent();
             list = Initials();
             Einkaufsliste.ItemsSource = list;
+            EinkaufslisteCheckbox.ItemsSource = list;
         }
 
         public List<string> Initials()
