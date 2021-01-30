d8a37e26688fc01a2a563b2a782fb1232b3c3f6e
PirngruberChris <christianpirngruber3@gmail.com>
2021-01-30 13:02:02 +0100

    created List for Selected Recipes with a butt which Navigates to the CookRecipeIngrediants Page which was created

---
+++ code/Reciply/Reciply/Models/SelectedRecipe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reciply.Models
+{
+    class SelectedRecipe
+    {
+        public string RecipeName { get; set; }
+    }
+}
