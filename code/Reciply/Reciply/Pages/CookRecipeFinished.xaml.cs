bb8243d964b4a993f501d1fa70e3e39a4f8fbbfb
michael0enkner <michael.enkner6@gmail.com>
2021-05-22 13:35:16 +0200

    Added CookRecipe

---
+++ code/Reciply/Reciply/Pages/CookRecipeFinished.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Reciply.Models;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Reciply.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CookRecipeFinished : ContentPage
+    {
+        private Recipe Recipe;
+        public CookRecipeFinished(Recipe recipe)
+        {
+            InitializeComponent();
+            Name.Text = recipe.Name;
+            Recipe = recipe;
+        }
+
+        private async void Home_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+
+        private async void Back_Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CookRecipePreparation(Recipe), true);
+        }
+    }
+}
--- code/Reciply/Reciply/Pages/MainPage.xaml.cs
+++ code/Reciply/Reciply/Pages/MainPage.xaml.cs
@@ -82,7 +82,26 @@ namespace Reciply
 
         private async void ButtonClicked_JetztKochen(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new CookRecipesIngrediants(), true);
+            //Demo Data for Testing => Later real recipe should be loaded
+            List<Ingredient> ingredients = new List<Ingredient>();
+            ingredients.Add(new Ingredient { Item = "Mehl", Amount = 1, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Kartoffeln", Amount = 2, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Schinken", Amount = 50, UnitOfMeasurement = UnitOfMeasurement.dag, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Eier", Amount = 2, UnitOfMeasurement = UnitOfMeasurement.Stück, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Pizzateig", Amount = 1, UnitOfMeasurement = UnitOfMeasurement.Pkg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Reis", Amount = 2, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Wasser", Amount = 5, UnitOfMeasurement = UnitOfMeasurement.l, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Essig", Amount = 2, UnitOfMeasurement = UnitOfMeasurement.Teelöffel, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Salz", Amount = 150, UnitOfMeasurement = UnitOfMeasurement.g, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Mais", Amount = 10, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Zuccini", Amount = 0.5, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Zucker", Amount = 12, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Karotten", Amount = 6, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Haferflocken", Amount = 7, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            ingredients.Add(new Ingredient { Item = "Pasta", Amount = 3, UnitOfMeasurement = UnitOfMeasurement.kg, IsSelected = false });
+            Recipe recipe = new Recipe { Id = 1, Description = "Ein supergeiles Gericht", Duration = 3, Name = "5 Minutenkuche", Portion = 4, Rating = 3, Preparation = "Einfach Mixen", Tags = "#Kuchen, #Schnell", Ingredient = ingredients };
+
+            await Navigation.PushAsync(new CookRecipeIngredients(recipe), true);
         }
 
         private async void Einkaufsliste_Edit_Clicked(object sender, EventArgs e)
